845aed7f070d5a877ad1dc4e1dac5f69189ba194
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-11-10 14:34:57 -0800

    requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='ms_identity_web',
-      version='0.13.15',
+      version='0.13.16',
       description='MSAL Identity Utilties',
       author='Azure Samples',
       url='https://github.com/azure-samples/ms-identity-python-utilities',
       packages=find_packages(),
+      requires=['msal>=1.6.0,<2'],
      )
 
 
