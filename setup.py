063b32ad187822dae9cc2c958a31539bfd7b5ff3
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-11-17 12:43:56 -0800

    increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ms_identity_web',
-      version='0.13.16',
+      version='0.13.17',
       description='MSAL Identity Utilties',
       author='Azure Samples',
       url='https://github.com/azure-samples/ms-identity-python-utilities',
