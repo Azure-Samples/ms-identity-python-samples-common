7c94c5fdf7e26a5111153ccb085d9120d8b9b502
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-12-09 19:22:46 -0800

    logger update

--- ms_identity_web/flask_blueprint/__init__.py
+++ ms_identity_web/flask_blueprint/__init__.py
@@ -12,6 +12,7 @@ from flask import (
 class FlaskAADEndpoints(Blueprint):
     def __init__(self, id_web):
         config = id_web.aad_config
+        logger = id_web._logger
         endpoints = config.flask.auth_endpoints
         prefix = endpoints.prefix
         name = prefix.strip('/')
@@ -19,15 +20,15 @@ class FlaskAADEndpoints(Blueprint):
 
         @self.route(endpoints.sign_in)
         def sign_in():
-            current_app.logger.info("test:::::::", request.url_rule)
+            logger.info(f"test::::::: {request.url_rule}")
             id_web.id_data.post_sign_in_url = request.values.get('post_sign_in_url', None)
-            current_app.logger.debug(f"{name}{endpoints.sign_in}: request received. will redirect browser to login")
+            logger.debug(f"{name}{endpoints.sign_in}: request received. will redirect browser to login")
             auth_url = id_web.get_auth_url(redirect_uri=url_for('.aad_redirect', _external=True))
             return redirect(auth_url)
 
         @self.route(endpoints.edit_profile)
         def edit_profile():
-            current_app.logger.debug(f"{name}{endpoints.edit_profile}: request received. will redirect browser to edit profile")
+            logger.debug(f"{name}{endpoints.edit_profile}: request received. will redirect browser to edit profile")
             auth_url = id_web.get_auth_url(
                     redirect_uri=url_for('.aad_redirect', _external=True),
                     b2c_policy=config.b2c.profile)
@@ -36,18 +37,19 @@ class FlaskAADEndpoints(Blueprint):
         @self.route(endpoints.redirect)
         def aad_redirect():
             post_sign_in_url = id_web.id_data.post_sign_in_url
-            current_app.logger.debug(f"{name}{endpoints.redirect}: request received. will process params")
+            logger.info(f"POST SIGN IN TEST URL IS {post_sign_in_url}")
+            logger.debug(f"{name}{endpoints.redirect}: request received. will process params")
             return id_web.process_auth_redirect(redirect_uri=url_for('.aad_redirect',_external=True),
                                                 afterwards_go_to_url=post_sign_in_url or url_for('index'))
 
         @self.route(endpoints.sign_out)
         def sign_out():
-            current_app.logger.debug(f"{name}{endpoints.sign_out}: signing out username: {g.identity_context_data.username}")
+            logger.debug(f"{name}{endpoints.sign_out}: signing out username: {g.identity_context_data.username}")
             return id_web.sign_out(url_for('.post_sign_out', _external = True))    # send the user to Azure AD logout endpoint
 
         @self.route(endpoints.post_sign_out)
         def post_sign_out():
-            current_app.logger.debug(f"{name}{endpoints.post_sign_out}: clearing session for username: {g.identity_context_data.username}")
+            logger.debug(f"{name}{endpoints.post_sign_out}: clearing session for username: {g.identity_context_data.username}")
             id_web.remove_user(g.identity_context_data.username)  # remove user auth from session on successful logout
             return redirect(url_for('index'))                   # take us back to the home page
         
